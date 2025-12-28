a2c80b8f399d8bd4829ed4f45fab9ff48cbd3c42
Pranav M <pranavmofficial2005@gmail.com>
2025-12-28 19:49:47 +0530

    fixed

--- src/containers/contact/Contact.js
+++ src/containers/contact/Contact.js
@@ -1,7 +1,6 @@
 import React, {useContext} from "react";
 import "./Contact.scss";
 import SocialMedia from "../../components/socialMedia/SocialMedia";
-import {illustration, contactInfo} from "../../portfolio";
 import {Fade} from "react-reveal";
 import StyleContext from "../../contexts/StyleContext";
 
